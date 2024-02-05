d576a72c274be87de82732f141e8350a68efbc2e
Hergott <emilien.herg@gmail.com>
2024-02-05 14:40:23 +0100

    branche header

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,23 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/map">Map</RouterLink>
-        <RouterLink to="/inscription">Inscription</RouterLink>
-        <RouterLink to="/connexion">Connexion</RouterLink>
-      </nav>
-    </div>
+    <nav class="flex flex-col border-2">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/map">Map</RouterLink>
+      <RouterLink to="/inscription">Inscription</RouterLink>
+      <RouterLink to="/connexion">Connexion</RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <RouterView/>
 </template>
-
-<style scoped>
-
-</style>
