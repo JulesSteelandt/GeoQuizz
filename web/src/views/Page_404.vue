e0dd5327090c868d7d638c8c01bcc24e34c951f7
Hergott <emilien.herg@gmail.com>
2024-02-05 09:57:42 +0100

    ajout page 404

---
+++ web/src/views/Page_404.vue
@@ -0,0 +1,14 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h2>Erreur #404</h2>
+    <RouterLink to="/">Retour à la home page</RouterLink>
+  </div>
+</template>
