79c9fa6da551f58eb2c957f6437e0ed47a4f23b0
NicoRiri <nicolate.bernardet@gmail.com>
2024-02-05 13:43:50 +0100

    dependencies : +setProfile

--- api/service_geoquizz/config/dependencies/action_dependencies.php
+++ api/service_geoquizz/config/dependencies/action_dependencies.php
@@ -2,12 +2,17 @@
 
 
 use geoquizz\service\app\actions\GetProfilAction;
+use geoquizz\service\app\actions\SetProfilAction;
 use Psr\Container\ContainerInterface;
 
 return[
 
     GetProfilAction::class => function (ContainerInterface $c){
         return new GetProfilAction($c->get('geoquizz.service'));
+    },
+
+    SetProfilAction::class => function (ContainerInterface $c){
+        return new SetProfilAction($c->get('geoquizz.service'));
     },
 
 ];
