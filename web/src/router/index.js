0c20a15552476d6c2f91637bcd4e9d08edc0cc34
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-05 12:15:02 +0100

    Frontend/feature/map (#2)
    * ajout page 404
    
    * ajout carte leaflet
    
    * conf: ajout de leaflet au package.json
    
    * conf: composant de test Map
    
    * conf: Route vers la map /Map
    
    ---------
    
    Co-authored-by: Hergott <emilien.herg@gmail.com>

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import Map from '@/components/Map.vue'
+import Page404 from '@/views/Page_404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: Map
+    },
+    {
+      path: '/:pathMatch(.*)',
+      name: 'page404',
+      component: Page404
     }
   ]
 })
