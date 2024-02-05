9d8ec5f5ac10bcdba672cf45b727b2de28d67ece
Nathan <nathan.melbeck@gmail.com>
2024-02-05 13:19:34 +0100

    fix: index.js

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import MapView from '@/views/MapView.vue'
-import Page404 from '@/views/Page_404.vue'
 import ConnexionView from "@/views/ConnexionView.vue";
 import InscriptionView from "@/views/InscriptionView.vue";
 import Map from '@/components/Map.vue'
@@ -24,6 +22,7 @@ const router = createRouter({
       path: '/inscription',
       name: 'inscription',
       component: InscriptionView
+    },
     {
       path: '/map',
       name: 'map',
