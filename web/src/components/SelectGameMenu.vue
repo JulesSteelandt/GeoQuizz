fb0c145eb80c6546a66c3b283c12c8a31cb8b60f
Nathan <nathan.melbeck@gmail.com>
2024-02-06 13:45:30 +0100

    feat: creation du composant SelectGameMenu.vue

---
+++ web/src/components/SelectGameMenu.vue
@@ -0,0 +1,33 @@
+<script >
+export default {
+  name: 'SelectGameMenu',
+  data() {
+    return {
+      serie: [
+          //ex : {id: 2, nom: "Nancy" },
+          //données d'exemple à remplacer par les données de l'API
+        {id: 1, nom: "Nancy" },
+        {id: 2, nom: "Metz" },
+        {id: 3, nom: "Paris" },
+        {id: 4, nom: "Lyon" },
+        {id: 5, nom: "Marseille" },
+      ],
+    }
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<template>
+  <section class="">
+
+  </section>
+
+</template>
+
+<style scoped>
+
+</style>
