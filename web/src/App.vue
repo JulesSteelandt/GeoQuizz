48d2f571bc6b5ae6529d616ff4899036980f5aa0
NathanMelbeck <94992898+NathanMelbeck@users.noreply.github.com>
2024-02-06 13:34:51 +0100

    Frontend/fix (#21)
    * fix: index.js
    
    * fix: bon routage
    
    * fix: home

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,39 @@
 <script>
-import {computed} from 'vue';
-import {useRoute, RouterView, RouterLink} from 'vue-router';
+import { computed } from 'vue';
+import { useRoute, RouterView, RouterLink } from 'vue-router';
 import PlayGeoQuizz from "@/components/playGeoQuizz.vue";
-import HeaderContent from "@/components/HeaderContent.vue";
 
 export default {
-  components: {HeaderContent, PlayGeoQuizz},
+  components: {PlayGeoQuizz},
   data() {
     return {
       isHomeRoute: true,
     };
   },
 
+
   /**
    * Vérifie si la route actuelle est la page d'accueil
-   * @returns boolean - true si la route actuelle est la page d'accueil, false sinon
+   * @returns {{isHomeRoute: ComputedRef<boolean>}} - true si la route actuelle est la page d'accueil, false sinon
    */
   setup() {
     const route = useRoute();
     const isHomeRoute = computed(() => route.path === '/');
 
-    return {isHomeRoute};
+    return { isHomeRoute };
   },
 
 };
 </script>
 
 <template>
-  <div v-if="isHomeRoute" class="flex flex-col">
-    <header class="bg-gray-700 w-screen w-1/5 flex flex-row items-center">
-      <!--
-      <div class="flex flex-row flex-wrap">
+  <header>
+    <div class="header flex flex-row flex-wrap justify-between p-1">
+      <div class="headerLogoText flex flex-row flex-wrap">
+        <!-- Logo à gauche -->
         <img class="w-28" src="@/components/icons/globe.png" alt="logo">
 
+        <!-- Textes à gauche -->
         <div class="flex-col ml-4 w-20">
           <div>
             <h1 class="text-7xl font-extrabold text-blue-500">
@@ -46,55 +47,50 @@ export default {
           </div>
         </div>
       </div>
-      -->
-
-      <header-content/>
-    </header>
-
-    <div class="flex flex-col justify-center w-full">
-      <playGeoQuizz/>
-      <div class="flex justify-center">
-        <!-- Bouton à droite -->
-        <RouterLink to="/map">
-          <button class="bg-blue-500 hover:bg-blue-900 text-white text-2xl font-bold py-2 px-4 rounded-xl mb-14 ">
-            Lancer le Quiz !
-          </button>
-        </RouterLink>
-
+      <div class="flex flex-row items-center" >
+        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl hover:bg-stone-400 hover:text-gray-700 mr-3">
+          <RouterLink to="/">
+            <button class="h-full w-full">Home</button>
+          </RouterLink>
+        </div>
+        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl hover:bg-stone-400 hover:text-gray-700 mr-3">
+          <RouterLink to="/guess">
+            <button class="h-full w-full">Jouer</button>
+          </RouterLink>
+        </div>
+        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl hover:bg-stone-400 hover:text-gray-700 mr-3">
+          <RouterLink to="/inscription">
+            <button class="h-full w-full">Inscription</button>
+          </RouterLink>
+        </div>
+        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl hover:bg-stone-400 hover:text-gray-700 mr-3">
+          <RouterLink to="/connexion">
+            <button class="h-full w-full">Connexion</button>
+          </RouterLink>
+        </div>
       </div>
-
-      <RouterView/>
-
-      <footer class="bg-stone-400 text-zinc-500 text-center p-4 flex flex-row justify-between w-full">
-        <p>GeoQuizz - 2024</p>
-        <p>Copyright IUT-Charlemagne</p>
-      </footer>
     </div>
-  </div>
 
-  <div v-else class="flex flex-row">
-    <header class="bg-gray-700 h-screen w-1/5 flex flex-col items-center">
-      <header-content/>
-    </header>
+    <div class="wrapper">
 
-    <div class="flex flex-col justify-center w-full">
-      <playGeoQuizz v-if="isHomeRoute"/>
-      <div class="flex justify-center" v-if="isHomeRoute">
-        <!-- Bouton à droite -->
-        <RouterLink to="/map">
-          <button class="bg-blue-500 hover:bg-blue-900 text-white text-2xl font-bold py-2 px-4 rounded-xl mb-14 ">
-            Lancer le Quiz !
-          </button>
-        </RouterLink>
-
-      </div>
-
-      <RouterView/>
-
-      <footer class="bg-stone-400 text-zinc-500 text-center p-4 flex flex-row justify-between w-full">
-        <p>GeoQuizz - 2024</p>
-        <p>Copyright IUT-Charlemagne</p>
-      </footer>
     </div>
-  </div>
+    <playGeoQuizz v-if="isHomeRoute"/>
+    <div class ="flex justify-center" v-if="isHomeRoute">
+      <!-- Bouton à droite -->
+      <RouterLink to="/guess">
+        <button class="bg-blue-500 hover:bg-blue-900 text-white text-2xl font-bold py-2 px-4 rounded-xl mb-14 ">
+          Lancer le Quiz !
+        </button>
+      </RouterLink>
+
+    </div>
+
+  </header>
+
+  <RouterView />
+
+  <footer class="bg-stone-400 text-zinc-500 text-center p-4 flex flex-row justify-between" >
+    <p>GeoQuizz - 2024</p>
+    <p>Copyrigtht IUT-Charlemagne</p>
+  </footer>
 </template>
