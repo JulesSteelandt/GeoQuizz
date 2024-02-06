08936d7cd2f008852e4574d022aeea3c307c7ead
julie <votre@email.com>
2024-02-06 13:07:22 +0100

    boutons lumineux

--- web/src/App.vue
+++ web/src/App.vue
@@ -58,12 +58,12 @@ export default {
             <button class="h-full w-full">Map</button>
           </RouterLink>
         </div>
-        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl hover:bg-stone-400 hover:text-gray-700 mr-3">
+        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl mr-3 bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
           <RouterLink to="/inscription">
             <button class="h-full w-full">Inscription</button>
           </RouterLink>
         </div>
-        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl hover:bg-stone-400 hover:text-gray-700 mr-3">
+        <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl mr-3 bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
           <RouterLink to="/connexion">
             <button class="h-full w-full">Connexion</button>
           </RouterLink>
