9bb74c7018b6464ce9aedee53c0d6fc2fab7590e
Jules <sonij590833@gmail.com>
2024-02-06 15:13:53 +0100

    feat: test WS notif

--- api/ws_geoquizz/src/server.js
+++ api/ws_geoquizz/src/server.js
@@ -1,14 +1,39 @@
-// server.js
+import { WebSocketServer } from 'ws';
 
-const http = require('http');
+const server = new WebSocketServer({ port: 3000 , clientTracking: true});
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, this is a basic Node.js server!\n');
+const notifyAll = (msg) =>{
+  server.clients.forEach((client_socket) => {
+    if (client_socket.readyState === WebSocket.OPEN)
+      client_socket.send(msg);
+  });
+}
+
+//const clients = [{clientSocket : ...., numCommande: ....} ]
+const clients = [ ]
+server.on('connection', (client_socket) => {
+  client_socket.addEventListener('message', (message) => {
+        clients.push({clientSocket: client_socket, numCommande : message.data})
+        client_socket.send('Vous êtes abonné au suivi de la commande numéro : ' + message.data );
+      }
+  )
 });
 
-const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.on('connection', function connection(ws) {
+  console.log('Client connected');
+
+  ws.on('message', function incoming(message) {
+    console.log('Received: %s', message);
+
+    server.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send('Un autre client a envoyé un message : ' + message);
+      }
+    });
+  });
+
+  ws.on('close', function close() {
+    console.log('Client disconnected');
+  });
 });
