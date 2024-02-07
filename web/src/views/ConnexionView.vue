8916cea8be72a9bcf7fa07156ad175ad71d276d5
julie <votre@email.com>
2024-02-07 10:54:57 +0100

    verifier email ok, autres vérification en cours

--- web/src/views/ConnexionView.vue
+++ web/src/views/ConnexionView.vue
@@ -3,11 +3,13 @@
 export default {
   data() {
     return {
-      isConnected: true,
+      isConnected: false,
       showError: false,
       showPassword: false,
       email: '',
       password: '',
+      isMail: true,
+      isFill: true,
     };
   },
   methods:{
@@ -21,16 +23,22 @@ export default {
       passwordInput.type = this.showPassword ? 'text' : 'password';
     },
 
+
     /**
      * Fonction pour s'authentifier, fetch l'api pour vérifier si l'utilisateur est bien inscrit
      * @returns {void} - Authentifie l'utilisateur
      */
     async login() {
       // Récupérer les valeurs des champs d'entrée
-      const email = this.$refs.emailInput.value;
-      const password = this.$refs.passwordInput.value;
+      const email = this.email;
+      const password = this.password;
 
       //const credentials = btoa(`${email}:${password}`);
+      // Vérifier si les champs email et password sont vides
+      if (email.trim() === '' || password.trim() === '') {
+        console.error('Veuillez entrer votre email et votre mot de passe');
+        return this.isFill = false;
+      }
 
       // Envoyer les données d'identification au serveur pour authentification
       try {
@@ -45,8 +53,7 @@ export default {
 
         // Vérifier la réponse du serveur
         if (response.message === 401) {
-          const data = await response.json();
-          console.log('Réponse du serveur:', data);
+          //const data = await response.json();
           // Authentification réussie
           console.log('Connexion réussie');
           this.isConnected = true;
@@ -71,7 +78,21 @@ export default {
     resetFields() {
       this.email = '';
       this.password = '';
-    }
+    },
+
+    /**
+     * Vérifie si l'email est valide
+     * @param {string} email - L'email à vérifier
+     * @returns {boolean} - true si l'email est valide, false sinon
+     */
+    verifEmail(email) {
+      // Vérifier l'email uniquement si l'utilisateur a interagi avec le champ email
+      if (this.emailTouched && email.trim() !== '') {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      }
+      return true;
+    },
 
   },
 }
@@ -83,22 +104,31 @@ export default {
   <div class="bg-gray-700 flex flex-col justify-center p-8 rounded-2xl m-auto">
     <div>
       <p class="text-white">Votre e-mail :</p>
-      <input ref="emailInput" class="w-60 mb-2.5 p-1 rounded-lg" type="text" placeholder="Votre e-mail ...">
+      <input v-model="email" class="w-60 mb-2.5 p-1 rounded-lg" type="text" placeholder="Votre e-mail ..." @input="emailTouched = true">
     </div>
     <div>
       <p class="text-white">Mot de passe :</p>
-      <input ref="passwordInput" class="w-60 mb-2.5 p-1 rounded-lg" type="password" placeholder="Votre mot de passe ..." @keyup.enter="login">
+      <input ref="passwordInput" v-model="password" class="w-60 mb-2.5 p-1 rounded-lg" type="password" placeholder="Votre mot de passe ..." @keyup.enter="login" >
       <button @click="togglePasswordVisibility" class="flex items-start ml-52 focus:outline-none">
         <img v-if="!showPassword" src="@/components/icons/invisible.png" alt="Masquer mot de passe" class="h-5 w-5 bg-transparent">
         <img v-else src="@/components/icons/visible.png" alt="Afficher mot de passe" class="h-5 w-5 bg-transparent">
       </button>
+      <p v-if="!verifEmail(email)" class="text-green-700 font-bold mb-2">L'email est invalide</p>
+
     </div>
       <button @click="login" class="text-white text-2xl font-bold mt-4 py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
          Je me connecte
       </button>
     </div>
-  <div v-if="!isConnected" class="flex flex-col items-center p-8 rounded-2xl m-auto">
-    <p class="text-red-500 text-xl font-bold ">La connexion a échoué, le nom d'utilisateur</p>
-    <p class="text-red-500 text-xl font-bold ">ou le mot de passe sont erronés</p>
+  <div v-if="showError" class="flex flex-col items-center p-8 rounded-2xl m-auto">
+    <p class="text-red-700 text-xl font-bold ">La connexion a échoué, le nom d'utilisateur</p>
+    <p class="text-red-700 text-xl font-bold ">ou le mot de passe sont erronés</p>
   </div>
+  <div v-else-if="isConnected" class="flex flex-col items-center p-8 rounded-2xl m-auto">
+    <p class="text-green-600 text-xl font-bold ">Connexion réussie</p>
+  </div>
+  <div v-else-if="!isConnected && !isFill" class="flex flex-col items-center p-8 rounded-2xl m-auto">
+    <p class="text-gray-400 text-xl font-bold ">Veuillez rentrer un email et un mot de passe</p>
+  </div>
+
 </template>
