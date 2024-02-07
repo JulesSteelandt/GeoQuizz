025c5b261fede2fb5cb2450e2ecd46f1db597292
EmilienHerg <101647769+EmilienHerg@users.noreply.github.com>
2024-02-07 13:11:50 +0100

    media queries (#41)

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,6 @@
 <script>
-import { computed } from 'vue';
-import { useRoute, RouterView, RouterLink } from 'vue-router';
+import {computed} from 'vue';
+import {useRoute, RouterView, RouterLink} from 'vue-router';
 import PlayGeoQuizz from "@/components/playGeoQuizz.vue";
 
 export default {
@@ -20,16 +20,16 @@ export default {
     const route = useRoute();
     const isHomeRoute = computed(() => route.path === '/');
 
-    return { isHomeRoute };
+    return {isHomeRoute};
   },
 
 };
 </script>
 
 <template>
-  <header>
-    <div class="header flex flex-row flex-wrap justify-between p-1 m-2">
-      <div class="headerLogoText flex flex-row flex-wrap">
+  <header class="bg-gray-700">
+    <div class="header flex flex-wrap p-1 m-2 lg:flex-row lg:justify-between max-lg:flex-col max-lg:items-center">
+      <div class="headerLogoText flex flex-row flex-wrap max-lg:mb-8 max-lg:mr-20">
         <!-- Logo à gauche -->
         <img class="w-28" src="@/components/icons/globe.png" alt="logo">
 
@@ -47,27 +47,32 @@ export default {
           </div>
         </div>
       </div>
-      <div class="flex flex-row items-center" >
-        <div class="text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
+      <div class="flex flex-row items-center">
+        <div
+            class="text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
           <RouterLink to="/">
-            <button class="h-full w-full">Home</button>
+            <button class="h-full w-full max-sm:text-base">Home</button>
           </RouterLink>
         </div>
-        <div class="text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
-          <RouterLink to="/selectgame">
-            <button class="h-full w-full">Jouer</button>
+        <div
+            class="text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
+          <RouterLink to="/guess">
+            <button class="h-full w-full max-sm:text-base max-sm:h-full">Jouer</button>
+
           </RouterLink>
         </div>
 
         <div class="notConnected flex flex-row items-center">
-          <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
+          <div
+              class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
             <RouterLink to="/inscription">
-              <button class="h-full w-full">Inscription</button>
+              <button class="h-full w-full max-sm:text-base">Inscription</button>
             </RouterLink>
           </div>
-          <div class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
+          <div
+              class="bg-blue-500 text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3">
             <RouterLink to="/connexion">
-              <button class="h-full w-full">Connexion</button>
+              <button class="h-full w-full max-sm:text-base">Connexion</button>
             </RouterLink>
           </div>
         </div>
@@ -75,26 +80,24 @@ export default {
         </div>
       </div>
     </div>
-
-    <div class="wrapper">
-
-    </div>
-    <playGeoQuizz v-if="isHomeRoute"/>
-    <div class ="flex justify-center" v-if="isHomeRoute">
-      <!-- Bouton à droite -->
-      <RouterLink to="/selectgame">
-        <button class="bg-blue-500 hover:bg-blue-900 text-white text-2xl font-bold py-2 px-4 rounded-xl mb-14 ">
-          Lancer le Quiz !
-        </button>
-      </RouterLink>
-
-    </div>
-
   </header>
 
-  <RouterView />
+  <playGeoQuizz v-if="isHomeRoute"/>
+  <div class="flex justify-center" v-if="isHomeRoute">
+    <!-- Bouton à droite -->
+    <RouterLink to="/guess">
+      <button class="bg-blue-500 hover:bg-blue-900 text-white text-2xl font-bold py-2 px-4 rounded-xl mb-14 ">
+        Lancer le Quiz !
+      </button>
+    </RouterLink>
 
-  <footer class="bg-stone-400 text-zinc-500 text-center p-4 flex flex-row justify-between" >
+  </div>
+
+
+
+  <RouterView/>
+
+  <footer class="bg-stone-400 text-zinc-500 text-center p-4 flex flex-row justify-between">
     <p>GeoQuizz - 2024</p>
     <p>Copyrigtht IUT-Charlemagne</p>
   </footer>
--- web/src/components/playGeoQuizz.vue
+++ web/src/components/playGeoQuizz.vue
@@ -1,14 +1,11 @@
-
 <script setup>
 
 
-defineProps({
-
-})
+defineProps({})
 </script>
 
 <template>
-<div class="playGeoQuizz w-1/2 rounded-xl flex flex-col overflow-hidden border-2 border-gray-100 mx-auto my-4">
+  <div class="playGeoQuizz w-1/2 rounded-xl flex flex-col overflow-hidden border-2 border-gray-100 mx-auto my-4">
 
     <!-- Contenu à gauche (image et texte) -->
     <div class="flex flex-col bg-stone-400 text-zinc-900">
@@ -16,12 +13,9 @@ defineProps({
       <img class="h-1/2" src="@/components/icons/fondAcc.jpg" alt="imageAcc">
 
       <!-- Texte en dessous de l'image -->
-      <p class="text-center py-5 text-4xl font-extrabold">Tester vos connaissances !</p>
+      <p class="text-center py-5 xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl font-extrabold">Tester vos connaissances !</p>
     </div>
-
-</div>
-
-
+  </div>
 </template>
 
 <style scoped>
