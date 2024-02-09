1ebf9e92e3c398aee29164fcfc4260cb244e2a84
julie <votre@email.com>
2024-02-10 00:25:16 +0100

    fixed CSS taille header

--- web/src/App.vue
+++ web/src/App.vue
@@ -94,7 +94,7 @@ export default {
           </RouterLink>
         </div>
         <div
-            class="text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3 hover:transition duration-300 ease-in-out transform hover:scale-105">
+            class=" max-sm:text-xs max-sm:mr-1.5 sm:text-base text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3 hover:transition duration-300 ease-in-out transform hover:scale-105">
           <RouterLink to="/gamemode">
             <button class="h-full w-full">Jouer</button>
           </RouterLink>
