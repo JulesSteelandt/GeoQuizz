81bbdfe851fdbad7142c97b2bbaac6d0e178dad2
julie <votre@email.com>
2024-02-09 10:51:43 +0100

    changement accueil pour mettre les règles du jeux

--- web/src/App.vue
+++ web/src/App.vue
@@ -122,7 +122,7 @@ export default {
               <RouterLink to="/connexion">
                 <button class="max-sm:text-xs max-sm:mr-1.5 sm:text-base text-white text-2xl font-bold py-2 px-4 rounded-xl bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 hover:bg-gradient-to-br shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 mr-3 hover:transition duration-300 ease-in-out transform hover:scale-105">Connexion</button>
                 <Connexion :is-connected="state.isConnected" />
-                <Connexion :is-connected="isConnected" @login-success="handleLoginSuccess" />
+                <Connexion :is-connected="state.isConnected" @login-success="handleLoginSuccess" />
               </RouterLink>
             </div>
 
